1aa5179dc0c5f269172e40e59d51b9bff1a28c43
Trung Lê <trung.tranle13@gmail.com>
2018-08-27 23:55:57 +0700

    Change Allowed Hosts

--- bakery/settings.py
+++ bakery/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'v+km%2us%=26@av=h#jrwoczz0eo33%l5zge2qur-*!zdx%t6_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.227.179.123',]
+ALLOWED_HOSTS = ['35.227.179.123','https://8000-dot-4333055-dot-devshell.appspot.com', '120.0.0.1',]
 
 
 # Application definition
